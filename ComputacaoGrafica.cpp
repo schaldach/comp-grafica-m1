786fd4961e9ae6f5b7a36be9ec5273b98af9f2e8
Gabriel Schaldach Morgado <72992857+schaldach@users.noreply.github.com>
2026-04-29 20:15:22 -0300

    voltando ao arquivo original, mudaod sem querer

--- ComputacaoGrafica.cpp
+++ ComputacaoGrafica.cpp
@@ -1,232 +1,320 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
 #include <GL/freeglut.h>
-#include <string>
-using namespace std;
+#include <vector>
+#include <tuple>
+#include <math.h>
+using vertice = std::tuple<double, double, double>;
+using lista_vertices = std::vector<vertice>;
+using aresta = std::pair<int, int>;
+using lista_arestas = std::vector<aresta>;
 
-//globals
+struct Cubo {
+	double tamanhoLado;
+	int numLados;
+	vertice posicao;
+	double escala;
+	double rotacao;
+	lista_vertices vertices;
+	lista_arestas arestas;
+};
 
-unsigned int elefante;
-vector<vector<float>> vertices;
-vector<vector<float>> normals;
-vector<vector<float>> textures;
-vector<vector<int>> faces;
+Cubo criar_cubo(double posicao_x, double posicao_y, double posicao_z, double tamanho_raio);
+void desenhar(Cubo poligono);
+void movimentar(Cubo& poligono, double distancia_x, double distancia_y, double distancia_z);
+void escalar(Cubo& poligono, double escala);
+void rotacionar(Cubo& poligono, double angulo, int eixo);
 
+void display();
+void redraw(int value);
 void keyboard(unsigned char key, int x, int y);
 
-void loadObj(string fname)
-{
-    int read;
-    float x, y, z;
-    ifstream arquivo(fname);
-    if (!arquivo.is_open()) {
-        cout << "arquivo nao encontrado";
-        exit(1);
-    }
-    else {
-        string tipo;
-        vector<float> ultima_face;
-        while (arquivo >> tipo)
-        {            
-            if (tipo == "v")
-            {
-                vector<float> vertice;
-                float x, y, z;
-                arquivo >> x >> y >> z;
-                vertice.push_back(x);
-                vertice.push_back(y);
-                vertice.push_back(z);
-                vertices.push_back(vertice);
-            }
+Cubo cubo;
+float velocidadeRotacao = 0;
+int delay = 10;
 
-            else if (tipo == "vn")
-            {
-                vector<float> normal;
-                float x, y, z;
-                arquivo >> x >> y >> z;
-                normal.push_back(x);
-                normal.push_back(y);
-                normal.push_back(z);
-                normals.push_back(normal);
-            }
-
-            else if (tipo == "vt")
-            {
-                vector<float> texture;
-                float x, y;
-                arquivo >> x >> y;
-                texture.push_back(x);
-                texture.push_back(y);
-                textures.push_back(texture);
-            }
-
-            else if (tipo == "f")
-            {
-                vector<int> face;
-                string x, y, z;
-                arquivo >> x >> y >> z;
-
-                int fp = stoi(x.substr(0, x.find("/")));
-                if (fp >= 0) fp = fp - 1; // o index negativo estará correto (-1 é de fato o último), enquanto que o index positivo, por começar em 1, tem que ser subtraido 1
-
-                int fs = stoi(y.substr(0, y.find("/")));
-                if (fs >= 0) fs = fs - 1;
-
-                int ft = stoi(z.substr(0, z.find("/")));
-                if (ft >= 0) ft = ft - 1;
-
-                face.push_back(fp);
-                face.push_back(fs);
-                face.push_back(ft);
-
-                ultima_face.clear();
-
-                ultima_face.push_back(face[0]);
-                ultima_face.push_back(face[1]);
-                ultima_face.push_back(face[2]);
-
-                faces.push_back(face);
-            }
-            // como detectar melhor quando continua a face? pois no radar existe uma linha com '/' que não é face
-            else if(tipo.find("/") != -1 && tipo.substr(tipo.find("/")+1, tipo.length()).find("/") != -1) { // ao menos 2 caracteres '/'
-                // o que seria o "tipo" veio um outro vértice, pois é uma face com mais de 3 vértices
-                // face com 4 vértices, criaremos o 2º triângulo
-                // se ele especifica um quadrilátero em qualquer ordem, é o 2º vértice que fica oposto ao último.
-                // então iremos fazer o triângulo com o 1º e 3º vértices
-                // e pensando nos casos com 5 vértices, que eu não havia percebido antes, esse comportamento se repete
-                vector<int> face;
-                // cout << "tipo: " << tipo << "\n";
-                int f = stoi(tipo.substr(0, tipo.find("/")));
-                if(f >= 0) f = f - 1; // o index negativo estará correto (-1 é de fato o último), enquanto que o index positivo, por começar em 1, tem que ser subtraido 1
-
-                face.push_back(ultima_face[0]);
-                face.push_back(ultima_face[2]);
-                face.push_back(f);
-
-                ultima_face.clear();
-
-                ultima_face.push_back(face[0]);
-                ultima_face.push_back(face[1]);
-                ultima_face.push_back(face[2]);
-
-                faces.push_back(face);
-            }
-        }
-    }
-
-
-
-
-    elefante = glGenLists(1);
-    glPointSize(2.0);
-    glNewList(elefante, GL_COMPILE);
-    {
-        glPushMatrix();
-        glBegin(GL_TRIANGLES);
-
-        for (int i = 0; i < faces.size(); i++)
-        {
-            vector<int> face = faces[i];
-
-            int v1_index = face[0];
-            if (v1_index < 0) v1_index = vertices.size() + v1_index;
-
-            int v2_index = face[1];
-            if (v2_index < 0) v2_index = vertices.size() + v2_index;
-
-            int v3_index = face[2];
-            if (v3_index < 0) v3_index = vertices.size() + v3_index;
-
-            glVertex3f(vertices[v1_index][0], vertices[v1_index][1], vertices[v1_index][2]);
-            glVertex3f(vertices[v2_index][0], vertices[v2_index][1], vertices[v2_index][2]);
-            glVertex3f(vertices[v3_index][0], vertices[v3_index][1], vertices[v3_index][2]);
-
-        }
-        glEnd();
-
-    }
-    glPopMatrix();
-    glEndList();
-    arquivo.close();
-
+void initGL() {
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); //Coloca a cor de background para preto e opaco
+	glClearDepth(1.0f);                   // Define o buffer de profundidade para o mais distante possível
+	glEnable(GL_DEPTH_TEST);   // Habilita o culling de profundidade
+	glDepthFunc(GL_LEQUAL);    // Define o tipo de teste de profundidade
 }
 
+void reshape(GLsizei width, GLsizei height) {
+	if (height == 0) height = 1;
+	GLfloat aspect = (GLfloat)width / (GLfloat)height;
 
-void reshape(int w, int h)
-{
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(60, (GLfloat)w / (GLfloat)h, 0.1, 1000.0);
+	glViewport(0, 0, width, height);
 
-    glMatrixMode(GL_MODELVIEW);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f, aspect, 0.1f, 100.0f);
 }
 
-float rotation_x = 0;
-float rotation_y = 0;
-float rotation_z = 0;
+int main(int argc, char** argv) {
 
-void drawElephant(float rotation)
-{
-    glPushMatrix();
-    glColor3f(1.0, 0.23, 0.27);
+	cubo = criar_cubo(0, 0, -5, 1);
 
-    // translação (x,y,z)
-    glTranslatef(0, -40.00, -105);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE);
 
-    // escala (mesmo número para todos)
-    glScalef(0.4, 0.4, 0.4);
+	glutInitWindowSize(640, 480);
+	glutInitWindowPosition(50, 50);
 
-    // rotação (x,y,z)
-    glRotatef(rotation_x, 1, 0, 0);
-    glRotatef(rotation_y, 0, 1, 0);
-    glRotatef(rotation_z, 0, 0, 1);
+	glutCreateWindow("3D");
 
-    glCallList(elefante);
-    glPopMatrix();
-}
-void display(void)
-{
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-    drawElephant(0);
-    glutSwapBuffers();
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	//glutSpecialFunc(keyboard_special);
+	glutTimerFunc(10, redraw, 0);
+	glutReshapeFunc(reshape);
+	initGL();
+
+	glutMainLoop();
+
+	return 0;
 }
 
-void timer(int value) {
-    glutPostRedisplay();
-    glutTimerFunc(10, timer, 0);
-}
+void display(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
 
-int main(int argc, char** argv)
-{
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(800, 450);
-    glutInitWindowPosition(20, 20);
-    glutCreateWindow("Carregar OBJ");
-    glutReshapeFunc(reshape);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(keyboard);
-    glutTimerFunc(10, timer, 0);
-    loadObj("data/porsche.obj");
-    glutMainLoop();
-    return 0;
+	glLoadIdentity();
+
+	desenhar(cubo);
+
+	glutSwapBuffers();
 }
 
 void keyboard(unsigned char key, int x, int y) {
-    std::cout << key;
-    switch (key) {
-    case 27:
-        exit(0);
-        break;
-    case 'o':
-        rotation_x += 2;
-        break;
-    case 'p':
-        rotation_x -= 2;
-        break;
-    }
+	std::cout << key;
+	switch (key) {
+	case 27:
+		exit(0);
+		break;
+	case 'o':
+		escalar(cubo, 0.9);
+		break;
+	case 'p':
+		escalar(cubo, 1.1);
+		break;
+
+	case 'w':
+		rotacionar(cubo, -0.1, 0);
+		break;
+	case 's':
+		rotacionar(cubo, 0.1, 0);
+		break;
+	case 'd':
+		rotacionar(cubo, -0.1, 1);
+		break;
+	case 'a':
+		rotacionar(cubo, 0.1, 1);
+		break;
+	case 'e':
+		rotacionar(cubo, -0.1, 2);
+		break;
+	case 'q':
+		rotacionar(cubo, 0.1, 2);
+		break;
+	case 'g':
+		movimentar(cubo, -0.1, 0, 0);
+		break;
+	case 'j':
+		movimentar(cubo, 0.1, 0, 0);
+		break;
+	case 'n':
+		movimentar(cubo, 0, -0.1, 0);
+		break;
+	case 'h':
+		movimentar(cubo, 0, 0.1, 0);
+		break;
+	case 'b':
+		movimentar(cubo, 0, 0, -0.1);
+		break;
+	case 'm':
+		movimentar(cubo, 0, 0, +0.1);
+		break;
+	}
+}
+
+void redraw(int value) {
+	glutPostRedisplay();
+	glutTimerFunc(delay, redraw, 0);
+}
+
+Cubo criar_cubo(double posicao_x, double posicao_y, double posicao_z, double tamanho_raio) {
+	Cubo novo_cubo;
+	std::get<0>(novo_cubo.posicao) = posicao_x;
+	std::get<1>(novo_cubo.posicao) = posicao_y;
+	std::get<2>(novo_cubo.posicao) = posicao_z;
+
+	novo_cubo.escala = 1;
+
+	novo_cubo.rotacao = 0;
+
+	double posicao_x_v, posicao_y_v, posicao_z_v;
+
+	posicao_x_v = posicao_x + tamanho_raio;
+	posicao_y_v = posicao_y + tamanho_raio;
+	posicao_z_v = posicao_z + tamanho_raio;
+	novo_cubo.vertices.push_back(vertice(posicao_x_v, posicao_y_v, posicao_z_v));
+
+	posicao_x_v = posicao_x - tamanho_raio;
+	posicao_y_v = posicao_y + tamanho_raio;
+	posicao_z_v = posicao_z + tamanho_raio;
+	novo_cubo.vertices.push_back(vertice(posicao_x_v, posicao_y_v, posicao_z_v));
+
+	posicao_x_v = posicao_x - tamanho_raio;
+	posicao_y_v = posicao_y + tamanho_raio;
+	posicao_z_v = posicao_z - tamanho_raio;
+	novo_cubo.vertices.push_back(vertice(posicao_x_v, posicao_y_v, posicao_z_v));
+
+	posicao_x_v = posicao_x + tamanho_raio;
+	posicao_y_v = posicao_y + tamanho_raio;
+	posicao_z_v = posicao_z - tamanho_raio;
+	novo_cubo.vertices.push_back(vertice(posicao_x_v, posicao_y_v, posicao_z_v));
+
+	posicao_x_v = posicao_x + tamanho_raio;
+	posicao_y_v = posicao_y - tamanho_raio;
+	posicao_z_v = posicao_z + tamanho_raio;
+	novo_cubo.vertices.push_back(vertice(posicao_x_v, posicao_y_v, posicao_z_v));
+
+	posicao_x_v = posicao_x - tamanho_raio;
+	posicao_y_v = posicao_y - tamanho_raio;
+	posicao_z_v = posicao_z + tamanho_raio;
+	novo_cubo.vertices.push_back(vertice(posicao_x_v, posicao_y_v, posicao_z_v));
+
+	posicao_x_v = posicao_x - tamanho_raio;
+	posicao_y_v = posicao_y - tamanho_raio;
+	posicao_z_v = posicao_z - tamanho_raio;
+	novo_cubo.vertices.push_back(vertice(posicao_x_v, posicao_y_v, posicao_z_v));
+
+	posicao_x_v = posicao_x + tamanho_raio;
+	posicao_y_v = posicao_y - tamanho_raio;
+	posicao_z_v = posicao_z - tamanho_raio;
+	novo_cubo.vertices.push_back(vertice(posicao_x_v, posicao_y_v, posicao_z_v));
+
+	for (int i = 0; i < 4; i++) {
+		novo_cubo.arestas.push_back(aresta(i, (i + 1) % 4));
+	}
+	for (int i = 0; i < 4; i++) {
+		novo_cubo.arestas.push_back(aresta(i + 4, ((i + 1) % 4) + 4));
+	}
+	for (int i = 0; i < 4; i++) {
+		novo_cubo.arestas.push_back(aresta(i, i + 4));
+	}
+
+	return novo_cubo;
+}
+
+void movimentar(Cubo& poligono, double distancia_x, double distancia_y, double distancia_z) {
+	std::cout << std::get<0>(poligono.posicao) << "\n";
+
+	std::get<0>(poligono.posicao) = std::get<0>(poligono.posicao) + distancia_x;
+	std::get<1>(poligono.posicao) = std::get<1>(poligono.posicao) + distancia_y;
+	std::get<2>(poligono.posicao) = std::get<2>(poligono.posicao) + distancia_z;
+
+	for (int i = 0; i < 8; i++) {
+		std::get<0>(poligono.vertices[i]) += distancia_x;
+		std::get<1>(poligono.vertices[i]) += distancia_y;
+		std::get<2>(poligono.vertices[i]) += distancia_z;
+	}
+
+	std::cout << std::get<0>(poligono.posicao) << "\n";
+}
+
+void escalar(Cubo& poligono, double escala) {
+	poligono.escala *= escala;
+
+	std::get<0>(poligono.posicao) = std::get<0>(poligono.posicao);
+	std::get<1>(poligono.posicao) = std::get<1>(poligono.posicao);
+	std::get<2>(poligono.posicao) = std::get<2>(poligono.posicao);
+
+	for (int i = 0; i < 8; i++) {
+		std::get<0>(poligono.vertices[i]) -= std::get<0>(poligono.posicao);
+		std::get<1>(poligono.vertices[i]) -= std::get<1>(poligono.posicao);
+		std::get<2>(poligono.vertices[i]) -= std::get<2>(poligono.posicao);
+
+		std::get<0>(poligono.vertices[i]) = std::get<0>(poligono.vertices[i]) * escala;
+		std::get<1>(poligono.vertices[i]) = std::get<1>(poligono.vertices[i]) * escala;
+		std::get<2>(poligono.vertices[i]) = std::get<2>(poligono.vertices[i]) * escala;
+
+		std::get<0>(poligono.vertices[i]) += std::get<0>(poligono.posicao);
+		std::get<1>(poligono.vertices[i]) += std::get<1>(poligono.posicao);
+		std::get<2>(poligono.vertices[i]) += std::get<2>(poligono.posicao);
+	}
+}
+
+void rotacionar(Cubo& poligono, double angulo, int eixo) {
+	double novoX;
+	double novoY;
+	double novoZ;
+
+	for (int i = 0; i < 8; i++) {
+		std::get<0>(poligono.vertices[i]) -= std::get<0>(poligono.posicao);
+		std::get<1>(poligono.vertices[i]) -= std::get<1>(poligono.posicao);
+		std::get<2>(poligono.vertices[i]) -= std::get<2>(poligono.posicao);
+
+		switch (eixo) {
+		case 0: // x
+			novoY = std::get<1>(poligono.vertices[i]) * cos(angulo) - std::get<2>(poligono.vertices[i]) * sin(angulo);
+			novoZ = std::get<1>(poligono.vertices[i]) * sin(angulo) + std::get<2>(poligono.vertices[i]) * cos(angulo);
+
+			// std::get<0>(poligono.vertices[i]) = std::get<0>(poligono.vertices[i]);
+			std::get<1>(poligono.vertices[i]) = novoY;
+			std::get<2>(poligono.vertices[i]) = novoZ;
+
+			break;
+		case 1: // y
+			novoX = std::get<0>(poligono.vertices[i]) * cos(angulo) - std::get<2>(poligono.vertices[i]) * sin(angulo);
+			novoZ = std::get<0>(poligono.vertices[i]) * sin(angulo) + std::get<2>(poligono.vertices[i]) * cos(angulo);
+
+			std::get<0>(poligono.vertices[i]) = novoX;
+			// std::get<1>(poligono.vertices[i]) = std::get<1>(poligono.vertices[i]);
+			std::get<2>(poligono.vertices[i]) = novoZ;
+
+			break;
+		case 2: // z
+			novoX = std::get<0>(poligono.vertices[i]) * cos(angulo) - std::get<1>(poligono.vertices[i]) * sin(angulo);
+			novoY = std::get<0>(poligono.vertices[i]) * sin(angulo) + std::get<1>(poligono.vertices[i]) * cos(angulo);
+
+			std::get<0>(poligono.vertices[i]) = novoX;
+			std::get<1>(poligono.vertices[i]) = novoY;
+			// std::get<2>(poligono.vertices[i]) = std::get<2>(poligono.vertices[i]);
+
+			break;
+		}
+
+		std::get<0>(poligono.vertices[i]) += std::get<0>(poligono.posicao);
+		std::get<1>(poligono.vertices[i]) += std::get<1>(poligono.posicao);
+		std::get<2>(poligono.vertices[i]) += std::get<2>(poligono.posicao);
+	}
+
+	//for (int i = 0; i < poligono.arestas.size(); i++) {
+	//	poligono.vertices[i].first -= poligono.posicao.first;
+	//	poligono.vertices[i].second -= poligono.posicao.second;
+
+	//	float novoX = poligono.vertices[i].first * cos(angulo) - poligono.vertices[i].second * sin(angulo);
+	//	float novoY = poligono.vertices[i].second = poligono.vertices[i].first * sin(angulo) + poligono.vertices[i].second * cos(angulo);
+
+	//	poligono.vertices[i].first = novoX;
+	//	poligono.vertices[i].second = novoY;
+
+	//	poligono.vertices[i].first += poligono.posicao.first;
+	//	poligono.vertices[i].second += poligono.posicao.second;
+	//}
+
+}
+
+void desenhar(Cubo poligono) {
+	glColor3f(1, 1, 1);
+	for (int i = 0; i < poligono.arestas.size(); i++) {
+		glBegin(GL_LINES);
+		float v_o = poligono.arestas[i].first;
+		float v_d = poligono.arestas[i].second;
+
+		glVertex3f(std::get<0>(poligono.vertices[v_o]), std::get<1>(poligono.vertices[v_o]), std::get<2>(poligono.vertices[v_o]));
+		glVertex3f(std::get<0>(poligono.vertices[v_d]), std::get<1>(poligono.vertices[v_d]), std::get<2>(poligono.vertices[v_d]));
+		glEnd();
+	}
 }
